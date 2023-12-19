c065f5f0721b509d54f5ddebfe45511bb052ef63
Ingolf Wagner <contact@ingolf-wagner.de>
2023-12-19 01:18:50 +0100

    some minor changes

--- nixos-mono/homes/your-name/i3.nix
+++ nixos-mono/homes/your-name/i3.nix
@@ -1,29 +1,32 @@
 { pkgs, lib, config, ... }:
 {
-  xsession.windowManager.i3 = {
+  xsession = {
     enable = true;
-    extraConfig = ''
-      default_border pixel
-    '';
-    config = {
-      modifier = "Mod4";
-      terminal = "alacritty";
-      focus = {
-        followMouse = true;
+    windowManager.i3 = {
+      enable = true;
+      extraConfig = ''
+        default_border pixel
+      '';
+      config = {
+        modifier = "Mod4";
+        terminal = "alacritty";
+        focus = {
+          followMouse = true;
+        };
+        startup = [{ command = "${pkgs.albert}/bin/albert"; always = true; }];
+        bars = [
+          {
+            #mode = "hide";
+            hiddenState = "hide";
+            position = "top";
+            workspaceButtons = true;
+            workspaceNumbers = true;
+            statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${config.home.homeDirectory}/.config/i3status-rust/config-blocks.toml";
+            fonts.size = 10.0;
+            trayOutput = "primary";
+          }
+        ];
       };
-      startup = [{ command = "${pkgs.albert}/bin/albert"; always = true; }];
-      bars = [
-        {
-          #mode = "hide";
-          hiddenState = "hide";
-          position = "top";
-          workspaceButtons = true;
-          workspaceNumbers = true;
-          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ${config.home.homeDirectory}/.config/i3status-rust/config-blocks.toml";
-          fonts.size = 10.0;
-          trayOutput = "primary";
-        }
-      ];
     };
   };
 
--- nixos-mono/homes/your-name/taskwarrior.nix
+++ nixos-mono/homes/your-name/taskwarrior.nix
@@ -2,13 +2,13 @@
 {
   programs.taskwarrior = {
     enable = true;
-    colorTheme = "solarized-dark-256.theme";
+    colorTheme = "solarized-dark-256";
     config = {
       rc.color = "off";
       report.active.filter = "+PENDING and ( +ACTIVE or +DUETODAY or +TODAY or +OVERDUE )";
       report.active.columns = [ "id" "start" "entry.age" "priority" "project" "due" "description" ];
       report.active.labels = [ "ID" "Started" "Age" "Priority" "Project" "Due" "Description" ];
     };
-    home.packages = [ pkgs.taskwarrior-tui ];
   };
+  home.packages = [ pkgs.taskwarrior-tui ];
 }
