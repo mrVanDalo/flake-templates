c70e73e007ab599b6a0e76f2bbe5490e2deac1bf
Ingolf Wagner <contact@ingolf-wagner.de>
2024-08-29 12:38:54 +0700

    :sparkles: add basic flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
-  outputs = { ... }: {
+  outputs = { self, ... }: {
+
+    defaultTemplate = self.templates.basic;
+
     templates = {
       nixos-mono = {
         path = ./nixos-mono;
         description = "NixOS Mono Repository";
+      };
+      basic = {
+        path = ./basic;
+        description = "basic nix flake repository";
       };
     };
   };
